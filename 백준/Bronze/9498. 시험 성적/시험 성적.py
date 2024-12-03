d727d1e00e18d6fe523eb8d0f98015fece2ddcae
HenryUN0504 <henrynam@bek.co.kr>
2024-12-03 18:04:14 +0900

    [Bronze V] Title: 시험 성적, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,11 @@
+var = int(input())
+if var >= 90 and var <= 100 :
+    print("A")
+elif var >= 80 and var <= 89 :
+    print("B")
+elif var >= 70 and var <= 79 :
+    print("C")
+elif var >= 60 and var <= 69 :
+    print("D")
+else :
+    print("F")
