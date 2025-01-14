553f57ca9dc35fdbdff192c1f584290a69b26e2e
HenryUN0504 <henrynam@bek.co.kr>
2025-01-14 18:18:07 +0900

    [Bronze V] Title: A+B - 8, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
@@ -0,0 +1,4 @@
+var2 = int(input())
+for i in range (var2):
+    var, var1 = map(int, input().split())
+    print("Case #" + str(i+1) + ": " + str(var) + " + " + str(var1) + " = " + str(var + var1))
